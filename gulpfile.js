6d278eba41640ef2b91e9837e3c8e77eb4553b57
Max Beatty <maxbeatty@users.noreply.github.com>
2016-08-17 11:30:54 -0700

    Update dependencies
    Fixes #3.

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const remoteSrc = require('gulp-remote-src');
 const replace = require('gulp-replace');
 const uglify = require('gulp-uglify');
 
-const BENCHMARKJS_VERSION = '2.1.0';
+const BENCHMARKJS_VERSION = require('./package.json').devDependencies.benchmark;
 const requestOptions = {
 	'gzip': true,
 	'strictSSL': true
